acab1f510a07ba105367daf80b3320fa8a2f2c94
Norman Matos <nmatos1100@gmail.com>
2021-05-05 17:12:28 +0000

    Update Customers Model to include total customer payments thus far

--- models/customers.sql
+++ models/customers.sql
@@ -1,56 +1,35 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
+    select * from {{ ref('stg_customers') }}
 ),
-
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
+    select * from {{ ref('orders') }}
 ),
-
-customer_orders as (
-
+customer_order_payments as (
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(coalesce(total_payment_amount,0)) as lifetime_value
     from orders
-
     group by 1
-
 ),
-
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        cop.first_order_date,
+        cop.most_recent_order_date,
+        coalesce(cop.number_of_orders, 0) as number_of_orders,
+        coalesce(cop.lifetime_value, 0) as lifetime_value
     from customers
-
-    left join customer_orders using (customer_id)
-
+    left join customer_order_payments as cop using (customer_id)
 )
-
 select * from final
